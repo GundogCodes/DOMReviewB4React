0f47a7c805f5ab0ed9746933a3a5c5774b286a4a
Gunish <gunishsharma20@gmail.com>
2023-07-11 16:58:29 -0600

    js done

---
+++ script.js
@@ -0,0 +1,38 @@
+//Constants
+document.getElementById('incrementAmount').defaultValue = 0
+//Cached Variables
+const plus = document.getElementById('plus')
+const minus = document.getElementById('minus')
+
+//State Variables
+let count = document.getElementById('count')
+let incrementVal = document.getElementById('incrementAmount')
+//Event Listeners
+plus.addEventListener('click', handlePlusClick)
+minus.addEventListener('click', handleMinusClick)
+//Functions
+function handlePlusClick(){
+    let addingAmount = parseInt(incrementVal.value)
+    let currentCount = parseInt(count.innerText)
+    console.log(currentCount)
+    currentCount = currentCount+ addingAmount
+    if(currentCount<0){
+        count.style.color = 'red'
+    } else if(currentCount>0){
+        count.style.color ='black' 
+    }
+    count.innerText = currentCount
+}
+
+function handleMinusClick(){
+    let minusAmount = parseInt(incrementVal.value)
+    let currentCount = parseInt(count.innerText)
+    console.log(currentCount)
+    currentCount = currentCount- minusAmount
+    if(currentCount<0){
+        count.style.color = 'red'
+    } else if(currentCount>0){
+        count.style.color ='black' 
+    }
+    count.innerText = currentCount
+}
